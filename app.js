54e097aebb43d5f9125aec9e7b408344303bc781
Rails Girls Novi Sad <railsgirlsnovisad@gmail.com>
2017-03-15 20:11:40 +0100

    Add multiple object to array and add endpoint all

--- app.js
+++ app.js
@@ -2,12 +2,23 @@ let express = require('express');
 let app = express();
 
 let event = {"id": 1, "title": "Marathon_Boston", "description": "This was a run",
-      "date": 12.06.2017};
+      "date": "12.06.2017"};
 
-
-
-/*
-app.get('/person', function (req, res) {
-  res.send(req.person);
+let array = [{"id": 1, "title": "Marathon_Boston", "description": "This was a run",
+      "date": "12.06.2017"}, 
+	  {"id": 2, "title": "Music_Festival", "description": "This was fun",
+      "date": "01.04.2017"}, 
+	  {"id": 3, "title": "Film_Festival", "description": "This was educational",
+      "date": "30.08.2017"}];
+	  
+app.get('/event', function (req, res) {
+  res.send(event);
 })
-*/
+
+app.get('/all', function (req, res) {
+	res.send(array);
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
