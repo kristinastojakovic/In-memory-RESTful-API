22b094090ee308474b1c34f86dd1cffda4e79abc
palaogn <palaogn@gmail.com>
2017-03-31 18:07:42 +0000

    minor fix in tests

--- test/test.js
+++ test/test.js
@@ -4,49 +4,52 @@ let server = require('../app.js');
 let should = chai.should();
 
 chai.use(chaiHttp);
-///jjjjjj
 
 
-/*
-* Test the /GET route
-*/
-describe('/GET events', () => {
-  it('it should list all the events', (done) => {
-	chai.request(server)
-		.get('/events')
-		.end((err, res) => {
-			res.should.have.status(200);
-			res.should.be.json;
-			res.body.should.be.a('array');
-			res.body.should.have.length(3);
-		  done();
-		});
-  });
 
-});
+describe('Events', () => {
+/*  beforeEach((done) => {
 
+    done();
+  }) */
 
-describe('/POST events', () => {
-	it('it should POST an event ', (done) => {
-		let my_event = {
-			"id" : '1',
-			"title" : "Marathon_Boston",
-			"description" : "This was a run",
-			"date" : "12.06.2017"
-		}
-		chai.request(server)
-			.post('/events')
-			.send(my_event)
-			.end((err, res) => {
-				res.should.have.status(200);
-				res.should.be.json;
-				res.body.should.be.a('array');
-				res.body.should.have.length(4);
-				res.body[3].should.have.property('id');
-				res.body[3].should.have.property('title');
-				res.body[3].should.have.property('description');
-				res.body[3].should.have.property('date');
-			  done();
-			});
-	});
+    describe('/GET events', () => {
+      it('it should list all the events', (done) => {
+    	chai.request(server)
+    		.get('/events')
+    		.end((err, res) => {
+    			res.should.have.status(200);
+    			res.should.be.json;
+    			res.body.should.be.a('array');
+    			res.body.should.have.length(3);
+    		  done();
+    		});
+      });
+    });
+
+    describe('/POST events', () => {
+    	it('it should POST an event ', (done) => {
+    		let my_event = {
+    			"id" : '1',
+    			"title" : "Marathon_Boston",
+    			"description" : "This was a run",
+    			"date" : "12.06.2017"
+    		}
+    		chai.request(server)
+    			.post('/events')
+    			.send(my_event)
+    			.end((err, res) => {
+    				res.should.have.status(200);
+    				res.should.be.json;
+    				res.body.should.be.a('array');
+    				res.body.should.have.length(4);
+    				res.body[3].should.have.property('id');
+    				res.body[3].should.have.property('title');
+    				res.body[3].should.have.property('description');
+    				res.body[3].should.have.property('date');
+    			  done();
+    			});
+    	});
+    });
+
 });
