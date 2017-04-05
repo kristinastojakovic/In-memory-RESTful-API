a57186b9dbc14979d4d3dfc34f7c4722b4df77bd
kristina.stojakovic@gmail.com <kristina.stojakovic@gmail.com>
2017-04-05 20:47:57 +0200

    Now you can insert new event to MongoDB.

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
-const express = require('express');
+const 	express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+var MongoClient = require('mongodb').MongoClient,
+	assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://localhost:27017/events';
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, db) {
+  
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  app.post('/events', (req, res) => {
+	  if (!req.body.title || !req.body.description || !req.body.date) {
+		res.statusCode = 404;
+		return res.send('You have to insert title, description and date');
+	  }
+	  
+	  insertDocuments(db, req, function() {
+		db.close();
+	  });
+	  
+	res.send("Success!");
+	  
+  })
+  
+});
 
 let array = [{"id": 1, "title": "Marathon_Boston", "description": "This was a run",
       "date": "12.06.2017"},
@@ -44,22 +71,18 @@ app.get('/events/:id', (req, res) => {
   res.send(event);
 })
 
-app.post('/events', (req, res) => {
-  if (!req.body.title || !req.body.description || !req.body.date) {
-    res.statusCode = 404;
-    return res.send('You have to insert title, description and date');
-  }
-
-  //makes new id for the new event
-  const newId = array[array.length-1].id + 1;
-  const newEvent = {"id": newId,
-              "title": req.body.title,
-              "description": req.body.description,
-              "date": req.body.date};
-
-  array.push(newEvent);
-  res.send(array);
-})
+var insertDocuments = function(db, req, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Insert some documents
+  collection.insertOne({"title": req.body.title, "description": req.body.description, "date": req.body.date}, function(err, result) {
+    assert.equal(err, null);
+    assert.equal(1, result.result.n);
+    assert.equal(1, result.ops.length);
+    console.log("Inserted 1 event into the collection");
+    callback(result);
+  });
+}
 
 app.put('/events/:id', (req, res) => {
  const id = parseInt(req.params.id);
